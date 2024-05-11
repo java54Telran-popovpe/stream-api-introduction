cc532b744245eb17809e42d5381843ea14af729f
Petr Popov <popov.pe@gmai.com>
2024-05-11 12:55:24 +0300

    Added \n in error message of LotoModelException class

--- src/telran/streams/LotoModelException.java
+++ src/telran/streams/LotoModelException.java
@@ -3,6 +3,6 @@ package telran.streams;
 @SuppressWarnings("serial")
 class LotoModelException extends Exception {
 	public LotoModelException( String argName, int givenValue, String rule ) {
-		super( String.format("Wrong argument <%s> = %d. %s", argName, givenValue, rule) );
+		super( String.format("Wrong argument <%s> = %d. %s\n", argName, givenValue, rule) );
 	}
 }
